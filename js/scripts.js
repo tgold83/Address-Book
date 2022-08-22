f891e7a71997c540eb79292f7964151f6d501840
tgold83 <ted.jonathan.gold@gmail.com>
2022-08-21 20:51:52 -0700

    add contact object and full name function

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+// Business Logic
+
+function Contact(firstName, lastName, phoneNumber) {
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.phoneNumber = phoneNumber;
+}
+
+Contact.prototype.fullName = function() {
+  return this.firstName + " " + this.lastName;
+};
